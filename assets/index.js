cdce7f91980751b043b575261eb54ad7a3325fcd
karthikn <karthikkat7777@gmail.com>
2024-03-16 12:44:22 +0530

    UPDATE:Site crash due to Big Input solved!

--- assets/index.js
+++ assets/index.js
@@ -8,20 +8,28 @@ let total = document.getElementById("total");
 function action(){
     iv = input_value.value;
     res_dice.textContent =`Random Dices Rolled: ${iv}`;
-    
-    values = [];
-    images = [];
-    for(i=1;i<=iv;i++){
-        rand = Math.floor(Math.random() *6)+1;
-        values.push(rand);
-        images.push(`<img src='./dice_images/dice${rand}.svg'>`);
-        
-    }
-    sum =0;
-    for (num of values){
-        sum+=num;
-    }
-    total.textContent = `Total: ${sum}`;
-    image_container.innerHTML = images.join(" ");
 
+    if (Number(iv) > 100){
+        res_dice.textContent = "Too Much Dices. Kanaku penna Kastama Irukuda dei😂. Kammiya Podu Nae!.JFF"
+    }else if(Number(iv) <0){
+        res_dice.textContent = "Dice Roll starts from 1. Minus lam Kedaiyathupa Thambi😂"
+    }else{
+        values = [];
+        images = [];
+        for(i=1;i<=iv;i++){
+            rand = Math.floor(Math.random() *6)+1;
+            values.push(rand);
+            images.push(`<img src='./dice_images/dice${rand}.svg'>`);
+            
+        }
+        sum =0;
+        for (num of values){
+            sum+=num;
+        }
+        total.textContent = `Total: ${sum}`;
+        image_container.innerHTML = images.join(" ");
+    
+    }
+    
+    
 }
